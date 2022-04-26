c83fb11b8e10576a132861134400ded469b71566
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-04-26 18:42:14 +0100

    Api fetch made.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
+import Movie from './components/view/Movie';
 
 function App() {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect (() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/tv/popular?api_key=9de90cd572615203d991274e9147bb20&language=en-US&page=1');
+    
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.results);
+  }; 
+
   return (
     <div className="App">
-
+    <div className="popular-movies">
+      {popular.map((movie) => {
+        return (
+           <Movie key={movie.id} movie={movie} />
+           )
+      })};
+    </div> 
     </div>
   );
 }
---
+++ src/components/view/Movie.js
@@ -0,0 +1,12 @@
+ import React from 'react';
+ 
+ function Movie({movie}) {
+   return (
+     <div>
+         <h2>Title</h2>
+         <img src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt="" />
+     </div>
+   )
+ }
+ 
+ export default Movie;
